a6be6e00dfb3e55cd4376b4b8baccb5748a6e10c
Faiyaz <faiyazrahman03@gmail.com>
2025-12-25 15:27:30 +0600

    feat: open live capture using pcap to capture raw bytes

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"time"
 
 	"github.com/faiyaz032/arp-scanner/pkg"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 const (
@@ -24,7 +28,52 @@ func main() {
 		log.Fatalf("Failed to find interface ip: %v", err)
 	}
 
-	usableIPs, networkIP, broadcastIP := pkg.GenerateIPs(ipnet)
+	_, networkIP, broadcastIP := pkg.GenerateIPs(ipnet)
+
+	// open live capture to capture raw bytes
+	handle, err := pcap.OpenLive(iface.Name, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatalf("Failed to open live capture: %v", err)
+	}
+	defer handle.Close()
+
+	// construct ethernet layer
+	eth := &layers.Ethernet{
+		SrcMAC:       iface.HardwareAddr,
+		DstMAC:       net.HardwareAddr{255, 255, 255, 255, 255, 255}, //broadcast mac
+		EthernetType: layers.EthernetTypeARP,
+	}
+
+	// construct arp layer
+	arp := &layers.ARP{
+		AddrType:        layers.LinkTypeEthernet,
+		Protocol:        layers.EthernetTypeIPv4,
+		HwAddressSize:   6,
+		ProtAddressSize: 4,
+		Operation:       layers.ARPRequest,
+
+		SourceHwAddress:   iface.HardwareAddr,
+		SourceProtAddress: ipnet.IP,
+
+		DstHwAddress:   []byte{0, 0, 0, 0, 0, 0},
+		DstProtAddress: []byte(net.ParseIP("192.168.0.101").To4()),
+	}
+
+	buffer := gopacket.NewSerializeBuffer()
+	options := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	err = gopacket.SerializeLayers(buffer, options, eth, arp)
+	if err != nil {
+		log.Fatalf("Failed to serialize packet: %v", err)
+	}
+
+	err = handle.WritePacketData(buffer.Bytes())
+	if err != nil {
+		log.Fatalf("Failed to write packet: %v", err)
+	}
 
 	fmt.Println("Interface Name:", iface.Name)
 	fmt.Println("IP:", ipnet.IP)
@@ -32,5 +81,4 @@ func main() {
 	fmt.Println("Source MAC:", iface.HardwareAddr.String())
 	fmt.Println("Netowork IP: ", networkIP)
 	fmt.Println("Broadcast IP: ", broadcastIP)
-	fmt.Println("Usable IPs: ", usableIPs)
 }
