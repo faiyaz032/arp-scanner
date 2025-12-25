35a2b56f7868f22d1f9fc8e0ffac53401ca8c46d
Faiyaz <faiyazrahman03@gmail.com>
2025-12-25 14:48:26 +0600

    feat: implement GenerateIPs function to get usable ips of a subnet

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/faiyaz032/arp-scanner/pkg"
+)
+
+const (
+	snapshotLen = 1024
+	promiscuous = false
+	timeout     = 1 * time.Second
 )
 
 func main() {
 	iface, err := pkg.GetActiveInterface()
 	if err != nil {
-		log.Fatal(err)
-		return
+		log.Fatalf("Failed to find active interface: %v", err)
+	}
+	ipnet, err := pkg.GetInterfaceIP(iface)
+	if err != nil {
+		log.Fatalf("Failed to find interface ip: %v", err)
 	}
 
+	usableIPs, networkIP, broadcastIP := pkg.GenerateIPs(ipnet)
+
+	fmt.Println("Interface Name:", iface.Name)
+	fmt.Println("IP:", ipnet.IP)
+	fmt.Println("IP Mask:", ipnet.Mask)
+	fmt.Println("Source MAC:", iface.HardwareAddr.String())
+	fmt.Println("Netowork IP: ", networkIP)
+	fmt.Println("Broadcast IP: ", broadcastIP)
+	fmt.Println("Usable IPs: ", usableIPs)
 }
--- pkg/network.go
+++ pkg/network.go
@@ -32,29 +32,73 @@ func GetActiveInterface() (*net.Interface, error) {
 	return nil, fmt.Errorf("no active interface found")
 }
 
-func GetInterfaceIP(iface *net.Interface) (net.IP, error) {
+func GetInterfaceIP(iface *net.Interface) (*net.IPNet, error) {
 	addrs, err := iface.Addrs()
-
 	if err != nil {
 		return nil, err
 	}
 
 	for _, addr := range addrs {
 		ipnet, ok := addr.(*net.IPNet)
-
 		if !ok {
 			continue
 		}
 
-		// extract the ip from ipnet struct
-		ip := ipnet.IP
-		ipv4 := ip.To4()
-
-		// if this is in ipv6, continue
+		ipv4 := ipnet.IP.To4()
 		if ipv4 == nil {
 			continue
 		}
 
-		return ip, nil
+		return &net.IPNet{
+			IP:   ipv4,
+			Mask: ipnet.Mask,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("no IPv4 address found on interface %s", iface.Name)
+}
+
+func GenerateIPs(ipnet *net.IPNet) (
+	usableIPs []net.IP,
+	networkIP net.IP,
+	broadcastIP net.IP,
+) {
+	ip := ipnet.IP.To4()
+	if ip == nil {
+		return nil, nil, nil
+	}
+
+	// netowrk IP
+	networkIP = ip.Mask(ipnet.Mask)
+
+	// collect all ips
+	var allIPs []net.IP
+	for ip := networkIP; ipnet.Contains(ip); incIP(ip) {
+		ipCopy := make(net.IP, len(ip))
+		copy(ipCopy, ip)
+		allIPs = append(allIPs, ipCopy)
+	}
+
+	if len(allIPs) < 2 {
+		return nil, networkIP, nil
+	}
+
+	// broadcastIP = last IP in subnet
+	broadcastIP = allIPs[len(allIPs)-1]
+
+	// remove network & broadcast
+	if len(allIPs) > 2 {
+		usableIPs = allIPs[1 : len(allIPs)-1]
+	}
+
+	return usableIPs, networkIP, broadcastIP
+}
+
+func incIP(ip net.IP) {
+	for i := len(ip) - 1; i >= 0; i-- {
+		ip[i]++
+		if ip[i] != 0 {
+			break
+		}
 	}
 }
